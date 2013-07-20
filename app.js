942536de963b48eac57baf962cd69a17e606d530
Steve Schroeder <tekknoschtev@gmail.com>
2013-07-19 22:37:51 -0400

    Setup, switching to EJS

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , mongoose = require('mongoose');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
